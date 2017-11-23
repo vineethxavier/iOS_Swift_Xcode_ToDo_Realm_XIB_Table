a7aaafb9a2e820ace10936e7259f22e938d467a4
Vineeth Xavier <vineethxavier89@gmail.com>
2017-11-23 13:04:36 -0500

    pod, import realm, googlesignin, realm and funny coredata ya both :P #1

---
+++ ToDoWithRealmGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoWithRealmGit
+//
+//  Created by Vineeth Xavier on 11/23/17.
+//  Copyright © 2017 Vineeth Xavier. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+import YouTubePlayer
+import GoogleSignIn
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+    }
+}
+

f3ee7f5345a430671a331c591bf17796e7423916
Vineeth Xavier <vineethxavier89@gmail.com>
2017-11-23 15:19:28 -0500

    - added toDOCell xib - added tabelview -added 2 array to tableview - xib has 2 label and 1 textview

--- ToDoWithRealmGit/ViewController.swift
+++ ToDoWithRealmGit/ViewController.swift
@@ -6,16 +6,45 @@
 //  Copyright © 2017 Vineeth Xavier. All rights reserved.
 //
 
+//toDoIdentifier
+
 import UIKit
 import CoreData //ya i am using reaml :D
 import RealmSwift
 import YouTubePlayer
 import GoogleSignIn
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tabViewDel: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-     
+        tabViewDel.dataSource = self
+        tabViewDel.delegate = self
+        
+        let nibName = UINib(nibName: "toDoCell", bundle: nil)
+        tabViewDel.register(nibName, forCellReuseIdentifier: "toDoIdentifier")
     }
+    
+    var itemNameTempArray:[String] = ["Apple","Ball","Cat","Dog","End"]
+    var itemNumberTemArray:[String] = ["1","2","3","4","5"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemNameTempArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tabViewDel.dequeueReusableCell(withIdentifier: "toDoIdentifier", for: indexPath) as! toDoCell
+        cell.commonInit(itemNumberTemArray[indexPath.row], itemNme: itemNameTempArray[indexPath.row])
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 86
+    }
+    
+
+
+
 }
 
---
+++ ToDoWithRealmGit/cell/toDoCell.swift
@@ -0,0 +1,33 @@
+//
+//  toDoCell.swift
+//  ToDoWithRealmGit
+//
+//  Created by Vineeth Xavier on 11/23/17.
+//  Copyright © 2017 Vineeth Xavier. All rights reserved.
+//
+
+import UIKit
+
+class toDoCell: UITableViewCell {
+
+    @IBOutlet weak var itemNumber: UILabel!
+    @IBOutlet weak var itemName: UILabel!
+    @IBOutlet weak var itemQuantity: UITextField!
+    
+    func commonInit(_ itemNo:String, itemNme:String ){
+        self.itemNumber.text = itemNo
+        self.itemName.text = itemNme
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
